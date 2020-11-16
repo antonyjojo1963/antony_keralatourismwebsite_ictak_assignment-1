23b5e3d08fdb3c7d742b25c6c2a1c92773d7fd5c
antony <antonyjojo1963@gmail.com>
2020-11-16 15:56:13 +0530

    First Commit : First Version

---
+++ form.js
@@ -0,0 +1,35 @@
+let email = document.getElementById("email");
+let pass = document.getElementById("pass");
+let pass1 = document.getElementById("pass1");
+
+console.log("hi")
+
+function validate(){
+    if(email.value.trim==""){
+        alert("Fields cannot be empty");
+        return false;
+    }
+    else if(pass.value.trim==""){
+        alert("Password cannot be empty");
+        return false;
+    }
+    else if(pass.value.length<=5){
+        alert("Password is too Short");
+        pwd.style.border="2px solid red";
+        return false;
+    }
+
+    else if (pass1.value != pass.value) {
+        alert("Password does not Match ");
+        pwd1.style.border="2px solid red";
+        return false;
+    }
+
+
+    else{
+        return true;
+    }
+
+
+
+}
